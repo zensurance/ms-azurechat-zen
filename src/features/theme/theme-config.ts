24c044e73ddc1d6fb2179b7e1f9b85995549270c
bwitzig-zen <brandon.witzig@zensurance.com>
2024-02-12 13:44:16 -0500

    Adding ' to end of system prompt to allow build to work

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -2,7 +2,6 @@ export const AI_NAME = "Zeed AI";
 export const AI_DESCRIPTION = "Azure Chat is a friendly AI assistant.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
-export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
-;
+export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.`;
 
 export const NEW_CHAT_NAME = "New chat";
