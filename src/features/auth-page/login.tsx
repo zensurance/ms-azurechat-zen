cbda26f0b27f5c0c29987f895cf80a6e79651185
bwitzig-zen <brandon.witzig@zensurance.com>
2024-09-12 11:55:02 -0400

    Adjusting login names

--- src/features/auth-page/login.tsx
+++ src/features/auth-page/login.tsx
@@ -27,13 +27,13 @@ export const LogIn: FC<LoginProps> = (props) => {
           <span className="text-primary">{AI_NAME}</span>
         </CardTitle>
         <CardDescription>
-          Login in with your Microsoft 365 account
+          Login with your Zensurance Employee account
         </CardDescription>
       </CardHeader>
       <CardContent className="grid gap-4">
         {/* <Button onClick={() => signIn("github")}>GitHub</Button> */}
-        <Button onClick={() => signIn("google")}>Google</Button>
-        <Button onClick={() => signIn("azure-ad")}> Microsoft 365</Button>
+        <Button onClick={() => signIn("google")}>Zensurance Employee</Button>
+        <Button onClick={() => signIn("azure-ad")}>Admin</Button>
 {/*         {props.isDevMode ? (
           <Button onClick={() => signIn("localdev")}>
             Basic Auth (DEV ONLY)
